ea2eb5b9ec6990a7f8961ed19359db6f0678886d
Sai Sirisha <siri.gene@gmail.com>
2019-06-10 20:42:13 +0530

    Added speech project

---
+++ speech_project/scripts/data_loading_v2.py
@@ -0,0 +1,46 @@
+from torch.utils.data import Dataset, DataLoader
+import os 
+import numpy
+
+class arctic_database(Dataset):
+
+   def __init__(self, src_dir, tgt_dir):
+    
+    print("i got src dir as", src_dir)
+    self.src_dir = src_dir
+    self.tgt_dir = tgt_dir
+    self.files = sorted(os.listdir(self.src_dir)) ### calling this in def __len__
+ 
+   def __len__(self):
+
+#    print("len is", len(self.files))  #### __len__
+    return len(self.files)
+
+   def __getitem__(self, idx): 
+#     print("am in get item")
+#     print((sorted(os.listdir(self.src_dir))[idx]))
+    src_files = (sorted(os.listdir(self.src_dir))[idx])
+    tgt_files = (sorted(os.listdir(self.tgt_dir))[idx])
+
+    src_filename = os.path.join(self.src_dir,src_files)   
+    tgt_filename = os.path.join(self.tgt_dir,tgt_files)
+    
+    src_info = numpy.loadtxt(src_filename)
+    tgt_info = numpy.loadtxt(tgt_filename)
+    #print("am opening the file", "index",src_info[idx], "info", src_info)#numpy.loadtxt(filename))
+    return src_info
+ #     return (sorted(os.listdir(self.src_dir))[idx])
+
+
+
+eng_data = arctic_database(src_dir='/home/siri/backup_on8june/Documents/Projects/Interspeech_2019/Data/baseline_40_samples/input_full', 
+                               tgt_dir='/home/siri/backup_on8june/Documents/Projects/Interspeech_2019/Data/baseline_40_samples/output_full')
+
+data = DataLoader(eng_data, batch_size=4, shuffle=True)
+
+#print("eng data len", [ p for p in eng_data[0]])
+for i, slt in enumerate(data):
+
+#    print("i is", i)
+    slt = data[i]
+    print("slt is", slt)
--- speech_project/scripts/readme.txt
+++ speech_project/scripts/readme.txt
@@ -8,3 +8,15 @@ resolved len issue
 June 9
 
 resolved get item issue
+
+June 10
+
+Reading the wavefiles in the function get item
+Added dataloader
+index thing--> 671*60 and 761*60 are stored as 60,1 which means stores info column wise (dimension wise) =====> apparently this is why i did padding before calling DataLoader in my LSTM code
+DataLoader --> does batching and shuffling --> https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
+
+1) return index --> ur input shape is changed
+2) use dictionary DataLoader needs tyhe input of same shape
+
+Task to do tranform the matrix shape before feeding it to the DataLoader
